9d7f5f2223e8e44cfd537d3ca09b7acc1f9f0178
vanessa-ariel <vanessa.ariel24@gmail.com>
2018-10-17 16:59:00 -0300

    Add progress bar, carousel and form

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let form = $('.form-contact');
+
+form.validate({
+    errorElement: 'span',
+    rules: {
+        name: 'required',
+        email: {
+            required: true,
+            email: true,
+        },
+        phone: 'required',
+        text: 'required'
+    },
+    messages: {
+        name: 'Please tell me who you are (:',
+        email: {
+            required: 'Please, type a valid email address',
+            email: 'Invalid email address'
+    },
+        phone: 'Please, type our phone number',
+        text: 'Please, type a message'
+    }
+})
